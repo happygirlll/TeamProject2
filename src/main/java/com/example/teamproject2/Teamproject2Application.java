7b99cd54672445d5d1b3f1d95ab7e06da19cdd6b
happygirlll <lbm000314@gmail.com>
2025-05-07 11:28:30 +0900

    initial commit

---
+++ src/main/java/com/example/teamproject2/Teamproject2Application.java
@@ -0,0 +1,13 @@
+package com.example.teamproject2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Teamproject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Teamproject2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/teamproject2/Teamproject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teamproject2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Teamproject2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
